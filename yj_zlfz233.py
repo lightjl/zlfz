5fda5644c272bc616d9968764c6d6f4d70f7dd72
bg <bg@example.com>
2016-08-30 14:14:08 +0800

    相对强度

--- yj_zlfz233.py
+++ yj_zlfz233.py
@@ -1,4 +1,7 @@
-from datetime import datetime  
+import time
+from datetime import date
+from datetime import datetime, timedelta
+
 per = 0.1
 # 过滤ST及其他具有退市标签的股票
 def filter_st_stock(initial_stocks):
@@ -25,6 +28,7 @@ def get_growth_stock( stock_list):
 
     now = datetime.now()  
     year = now.year
+    month = now.month
     list_stock = list(df_Growth_PE_G.loc[:,'code'])
     
     #K70 	房地产业，去掉
@@ -41,7 +45,19 @@ def get_growth_stock( stock_list):
 	            indicator.inc_operation_profit_year_on_year
                  ).filter(valuation.code.in_(list_stock))
     yearP = [get_fundamentals(q_PE_G2, statDate=str(yearL[i])) for i in range(5)]
-    #print yearP[1]
+    
+    last_month = date(datetime.now().year,datetime.now().month,1)-timedelta(1) 
+    last_2month = date(datetime.now().year,datetime.now().month-1,1)-timedelta(1)
+    last_year = date(datetime.now().year-1,datetime.now().month,1)-timedelta(1) 
+    dp_price = get_price('000001.XSHG', start_date=last_year, end_date=last_month+timedelta(1) , frequency='daily', fields='close')
+    while last_month not in dp_price.index:
+        last_month = last_month-timedelta(1) 
+    while last_2month not in dp_price.index:
+        last_2month = last_2month-timedelta(1) 
+    while last_year not in dp_price.index:
+        last_year = last_year-timedelta(1)
+    # print dp_price
+    # print yearP[1]
     #todo： 去年全部 年报已公开，明年昨日再改
     #columns=[u'名称']+['%d EPS'% (yearL[3-i]) for i in range(4)] 2013 2014 2015
     # yearP0 = get_fundamentals(q_PE_G2, statDate=str(yearL[0]))
@@ -50,33 +66,41 @@ def get_growth_stock( stock_list):
     for i in list_stock:
         
         # print yearP[0][(yearP[0].code==i)]
-        print i
+        # print i
         flag_empty = False
         for j in range(4):
             if yearP[j][yearP[j].code==i].empty:
                 flag_empty = True
-            #print yearP[j][(yearP[j].code==i)]
         if flag_empty:
             continue
         
         eps = [1]*4
         for j in range(4):
-            eps[j] = yearP[j][yearP[j].code==i].iloc[0,3]
+            eps[j] = yearP[j][yearP[j].code==i]['eps'].values
         # eps = [ yearP[j][ (yearP[j].code==i).loc[0, 'eps'] ] for j in range(4) ]
         # print eps
         cap = [1]*4
         for j in range(4):
-            cap[j] = yearP[j][yearP[j].code==i].iloc[0, 1]
+            cap[j] = yearP[j][yearP[j].code==i]['capitalization'].values
             
         flag = False        
-        for i in range(3):
-            if eps[i]*cap[i] < (1+per)*eps[i+1]*cap[i+1]:
+        for j in range(3):
+            if eps[j]*cap[j] < (1+per)*eps[j+1]*cap[j+1]:
                 break
             flag = True    
         if flag:
-            print eps
-            results.append([i] + [['%.2f' %eps[3-j]*cap[3-j]/cap[0] for j in range(3) ]] )
-            print results
+            #results.append([['%.2f'%eps[3-j]*cap[3-j]/cap[0]  for j in range(3)]])
+            gg_price = get_price(i, start_date=last_year, end_date=datetime.now(), frequency='daily', fields='close')
+            dpqd1 = (dp_price['close'][last_month]-dp_price['close'][last_2month])/dp_price['close'][last_2month]
+            ggqd1 = (gg_price['close'][last_month]-gg_price['close'][last_2month])/gg_price['close'][last_2month]
+            dpqd12 = (dp_price['close'][last_month]-dp_price['close'][last_year])/dp_price['close'][last_year]
+            ggqd12 = (gg_price['close'][last_month]-gg_price['close'][last_year])/gg_price['close'][last_year]
+            xdqd12 = (ggqd12 - dpqd12)/ abs(dpqd12)
+            xdqd1 = (ggqd1 - dpqd1)/ abs(dpqd1)
+            #+ datetime.timedelta(days = -1)
+            results.append([i] + [xdqd1, xdqd12] + ['%.2f'%(eps[3-j]*cap[3-j]/cap[0])  for j in range(4)])
+            # print last_year
+    print results
 # 获得买入信号
 # 输出：list_to_buy为list类型,表示待买入的g.num_stocks支股票
 def stocks_can_buy():
